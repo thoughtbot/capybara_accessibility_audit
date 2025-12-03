f05e54436dade0a32d1d9a003c3d7811e5d46dc4
Daniel Gasienica <daniel@gasienica.ch>
2025-12-03 15:49:35 +0100

    Assert deep equality

--- test/system/reporter_golden_test.rb
+++ test/system/reporter_golden_test.rb
@@ -40,26 +40,7 @@ class ReporterGoldenTest < ApplicationSystemTestCase
     golden_json = File.read(GOLDEN_FILE)
     expected_data = JSON.parse(golden_json, symbolize_names: true)
 
-    assert_equal expected_data.keys.sort, normalized_actual.keys.sort, "Top-level keys don't match"
-    assert_equal expected_data[:summary].keys.sort, normalized_actual[:summary].keys.sort, "Summary keys don't match"
-
-    expected_page = expected_data[:violations_by_page].first
-    actual_page = normalized_actual[:violations_by_page].first
-
-    assert_equal expected_page[:violations].count, actual_page[:violations].count, "Number of violations doesn't match"
-
-    expected_violation = expected_page[:violations].first
-    actual_violation = actual_page[:violations].first
-
-    assert_equal expected_violation.keys.sort, actual_violation.keys.sort, "Violation keys don't match"
-    assert_equal expected_violation[:id], actual_violation[:id], "Violation ID doesn't match"
-    assert_equal expected_violation[:impact], actual_violation[:impact], "Violation impact doesn't match"
-
-    assert expected_data[:violations_by_rule].any?, "Expected violations_by_rule to have entries"
-    expected_rule = expected_data[:violations_by_rule].values.first
-    actual_rule = normalized_actual[:violations_by_rule].values.first
-
-    assert_equal expected_rule.keys.sort, actual_rule.keys.sort, "Rule keys don't match"
+    assert_equal expected_data, normalized_actual
   end
 
   test "JSON output with no violations matches empty golden file" do
@@ -89,11 +70,7 @@ class ReporterGoldenTest < ApplicationSystemTestCase
     expected_data = JSON.parse(golden_json, symbolize_names: true)
 
     # Compare structure
-    assert_equal expected_data.keys.sort, normalized_actual.keys.sort, "Top-level keys don't match"
-    assert_equal 0, normalized_actual[:summary][:total_violations], "Expected 0 violations"
-    assert_equal 0, normalized_actual[:summary][:num_pages_with_violations], "Expected 0 pages with violations"
-    assert_equal [], normalized_actual[:violations_by_page], "Expected empty violations_by_page"
-    assert_equal({}, normalized_actual[:violations_by_rule], "Expected empty violations_by_rule")
+    assert_equal expected_data, normalized_actual
   end
 
   private
